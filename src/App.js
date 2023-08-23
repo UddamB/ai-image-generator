140ae9a032b9c3dc79e81287880e483e105a701a
Uddam Bhathal <Uddamsingh14@gmail.com>
2023-08-23 01:31:31 -0400

    Created css file with homepage display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { ImageGenerator } from './Components/ImageGenerator/ImageGenerator';
 
 function App() {
   return (
     <div>
-
+      <ImageGenerator/> {/* Displaying ImageGenerator.jsx file on homepage */}
     </div>
   );
 }
---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ImageGenerator.css' /* Importing css file */
+
+export const ImageGenerator = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
